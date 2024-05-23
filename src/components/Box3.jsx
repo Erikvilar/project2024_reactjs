7088b0addd2d7e03aa0b175de0b39d4f2f0a4703
Erik Alves Vilar <91624090+Erikvilar@users.noreply.github.com>
2024-05-23 19:05:16 -0300

    Update Box3.jsx

--- src/components/Box3.jsx
+++ src/components/Box3.jsx
@@ -97,7 +97,7 @@ function Box3() {
           <Card
             class="project"
             title="Back-End"
-            link="src/components/images/img_pages/project1BackEnd.png"
+            link="src/components/images/img_pages/project1Backend.png"
 
           />
           <Card
